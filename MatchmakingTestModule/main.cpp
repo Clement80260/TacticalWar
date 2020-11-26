d8217c399ba05af7466a0f3b2f168f556b587980
mfresi <mfresi@la-providence.net>
2020-11-26 22:30:32 +0100

    1er tour casi fonctionnel

--- MatchmakingTestModule/main.cpp
+++ MatchmakingTestModule/main.cpp
@@ -63,9 +63,10 @@ int main(int argc, char ** argv)
 			for (int i = 0; i < equipe; i++)
 			{
 				if (teamIdToPlayers == TeamRest) {
-					
+					// Si une équipe n'a pas d'adversaire on mes l'équipe sur le banc sinon on l'a fait joué contre une autre équipe.
 					if (TeamRest.find(indexTeam + 1) == TeamRest.end())
 					{
+						indexTeam = TeamRest.size();
 						cout << "Tour " << indexTour << " - ";
 						cout << "equipe " << TeamRest[indexTeam][0]->getTeamNumber() << " sur le banc ! ;)" << endl;
 						TeamWinMatch[i+1] = TeamRest[indexTeam];
@@ -73,24 +74,54 @@ int main(int argc, char ** argv)
 					}
 					else
 					{
-					
 						cout << "Tour " << indexTour << " - ";
 						cout << "equipe " << TeamRest[indexTeam][0]->getTeamNumber() << " vs " << "equipe " << TeamRest[indexTeam + 1][0]->getTeamNumber() << endl;
+						// On force la 1ere equipe à win.
 						TeamWinMatch[i+1] = TeamRest[indexTeam];
+						// On force la seconde à lose.
 						TeamLosMatch[i+1] = TeamRest[indexTeam + 1];
 						indexTeam += 2;
 					}
-					
 				}
 				else
 				{
 					if (teamIdToPlayers.size() <= indexTeam) {
 						indexTeam = 1;
 					}
-						
 					if(TeamRest.size() & 1)
 					{
-						cout << "impaire" << endl;
+						TeamWinMatch.clear();
+						int idmax = TeamRest.size();
+						int copyindexTeam = 1, xy = 0;
+						for (int y = 0; y < i; y++) {
+							if (TeamRest.size() & 1) {
+								int indexcopy = copyindexTeam + 2;
+								if (TeamRest.find(indexcopy) == TeamRest.end())
+								{
+									cout << "Tour " << indexTour << " - ";
+									cout << "equipe " << TeamRest[indexTeam][0]->getTeamNumber() << " sur le banc ! ;)" << endl;
+									TeamWinMatch[xy + 1] = TeamRest[indexTeam];
+									TeamRest = TeamWinMatch;
+									indexcopy = 0;
+									copyindexTeam = 1;
+									idmax = TeamRest.size();
+									indexTeam = 1;
+									xy = 0;
+									TeamWinMatch.clear();
+								}
+								else 
+								{
+									cout << "Tour " << indexTour << " - ";
+									cout << "equipe " << TeamRest[idmax][0]->getTeamNumber() << " vs " << "equipe " << TeamRest[indexTeam][0]->getTeamNumber() << endl;
+									TeamWinMatch[xy + 1] = TeamRest[idmax];
+									TeamLosMatch[i + 1] = TeamRest[indexTeam];
+									indexTeam += 1;
+									idmax = TeamRest.size() - indexTeam + 1;
+									xy += 1;
+									copyindexTeam = indexTeam + 1;
+								}
+							}
+						}
 					}
 					else
 					{
@@ -101,8 +132,9 @@ int main(int argc, char ** argv)
 							if (TeamRest.size() & 1)
 							{
 								idmax = 1;
+								// TODO calcul à faire. (ptetre)
 							}
-							
+							// Tant qu'il ne reste pas une equipe en jeu onn continu de faire des matchs.
 							if (TeamRest.size() == 1) {
 								indexTeam = 1;
 								cout << "Win de l'equipe " << TeamRest[indexTeam][0]->getTeamNumber() << endl;
@@ -127,7 +159,6 @@ int main(int argc, char ** argv)
 									}
 								}
 							}
-
 						}
 						i = equipe;
 						x = equipe - 1;
