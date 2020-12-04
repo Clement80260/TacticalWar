f0f9635646baa5ef6f91da7db7de1959085f87a8
mfresi <mfresi@la-providence.net>
2020-12-04 21:23:27 +0100

    interface admin clean

--- TacticalWar/AdminScreen.cpp
+++ TacticalWar/AdminScreen.cpp
@@ -36,6 +36,11 @@ AdminScreen::AdminScreen(tgui::Gui * gui)
 	m_matchListpanel->setInheritedFont(font);
 	m_matchListpanel->getRenderer()->setBackgroundColor(sf::Color(128, 128, 128));
 
+	m_matchListCreate = tgui::ScrollablePanel::create();
+	m_matchListCreate->setSize(300, 600);
+	m_matchListCreate->setInheritedFont(font);
+	m_matchListCreate->getRenderer()->setBackgroundColor(sf::Color(128, 128, 128));
+
 	listTeam1 = tgui::ListBox::create();
 	listTeam1->setSize(200, 150);
 	listTeam1->setInheritedFont(font);
@@ -46,11 +51,40 @@ AdminScreen::AdminScreen(tgui::Gui * gui)
 	listTeam2->setInheritedFont(font);
 	listTeam2->getRenderer()->setBackgroundColor(sf::Color::White);
 
+	listMatchCreate = tgui::ListBox::create();
+	listMatchCreate->setSize(250, 550);
+	listMatchCreate->setInheritedFont(font);
+	listMatchCreate->getRenderer()->setBackgroundColor(sf::Color::White);
+
 	versus = tgui::Label::create();
 	versus->setInheritedFont(font);
 	versus->setTextSize(50);
 	versus->getRenderer()->setTextColor(sf::Color(240, 139, 27));
 	versus->setText("VS");
+
+	nameMatch = tgui::Label::create();
+	nameMatch->setInheritedFont(font);
+	nameMatch->setTextSize(15);
+	nameMatch->getRenderer()->setTextColor(sf::Color(240, 139, 27));
+	nameMatch->setText("nom du match :");
+
+	team1Choice = tgui::Label::create();
+	team1Choice->setInheritedFont(font);
+	team1Choice->setTextSize(15);
+	team1Choice->getRenderer()->setTextColor(sf::Color(240, 139, 27));
+	team1Choice->setText("equipe 1 :");
+
+	team2Choice = tgui::Label::create();
+	team2Choice->setInheritedFont(font);
+	team2Choice->setTextSize(15);
+	team2Choice->getRenderer()->setTextColor(sf::Color(240, 139, 27));
+	team2Choice->setText("equipe 2 :");
+
+	matchCreate = tgui::Label::create();
+	matchCreate->setInheritedFont(font);
+	matchCreate->setTextSize(20);
+	matchCreate->getRenderer()->setTextColor(sf::Color::Yellow);
+	matchCreate->setText("Matchs créés :");
 
 	createMatch = tgui::Button::create();
 	createMatch->setSize(150, 75);
@@ -61,14 +95,26 @@ AdminScreen::AdminScreen(tgui::Gui * gui)
 	//createMatch->getRenderer()->setBackgroundColor(sf::Color(90, 182, 96)); // couleur verte
 	createMatch->setText("Creer");
 
+	matchName = tgui::EditBox::create();
+	matchName->setSize(250, 30);
+	matchName->setInheritedFont(font);
+	matchName->getRenderer()->setBackgroundColor(sf::Color::White);
+
 
 
 	gui->add(matchPanelTitle);
 	gui->add(m_matchListpanel);
+	gui->add(m_matchListCreate);
 	gui->add(listTeam1);
 	gui->add(listTeam2);
 	gui->add(versus);
 	gui->add(createMatch);
+	gui->add(matchName);
+	gui->add(nameMatch);
+	gui->add(matchCreate);
+	gui->add(team1Choice);
+	gui->add(team2Choice);
+	gui->add(listMatchCreate);
 
 	LinkToServer::getInstance()->addListener(this);
 }
@@ -84,10 +130,17 @@ void AdminScreen::handleEvents(sf::RenderWindow * window, tgui::Gui * gui)
 	subtitle.setPosition(window->getSize().x / 2 - subtitle.getLocalBounds().width / 2, 10 + 128 + 10);
 	matchPanelTitle->setPosition(window->getSize().x / 2.0 - m_matchListpanel->getSize().x / 2.0, 270);
 	m_matchListpanel->setPosition(window->getSize().x / 2.0 - m_matchListpanel->getSize().x / 2.0, 300);
+	m_matchListCreate->setPosition(window->getSize().x / 2.0 + 700 - m_matchListCreate->getSize().x / 2.0, 300);
 	listTeam1->setPosition(window->getSize().x / 2.0 - 350 - listTeam1->getSize().x / 2.0, 450);
-	listTeam2->setPosition(window->getSize().x / 2.0 - 350 - listTeam2->getSize().x / 2.0, 750);
+	listTeam2->setPosition(window->getSize().x / 2.0 + 350 - listTeam2->getSize().x / 2.0, 450);
+	listMatchCreate->setPosition(window->getSize().x / 2.0 + 700 - listMatchCreate->getSize().x / 2.0, 330);
 	versus->setPosition(window->getSize().x / 2.0 - versus->getSize().x / 2.0, 500);
 	createMatch->setPosition(window->getSize().x / 2.0 - createMatch->getSize().x / 2.0, 800);
+	matchName->setPosition(window->getSize().x / 2.0 - matchName->getSize().x / 2.0, 350);
+	nameMatch->setPosition(window->getSize().x / 2.0 - 230 - nameMatch->getSize().x / 2.0, 353);
+	matchCreate->setPosition(window->getSize().x / 2.0 + 650 - matchCreate->getSize().x / 2.0, 270);
+	team1Choice->setPosition(window->getSize().x / 2.0 - 350 - team1Choice->getSize().x / 2.0, 425);
+	team2Choice->setPosition(window->getSize().x / 2.0 + 350 - team2Choice->getSize().x / 2.0, 425);
 
 	sf::Event event;
 	while (window->pollEvent(event))
--- TacticalWar/AdminScreen.h
+++ TacticalWar/AdminScreen.h
@@ -12,10 +12,17 @@ private:
 
 	tgui::Label::Ptr matchPanelTitle;
 	tgui::Label::Ptr versus;
+	tgui::Label::Ptr nameMatch;
+	tgui::Label::Ptr team1Choice;
+	tgui::Label::Ptr team2Choice;
+	tgui::Label::Ptr matchCreate;
 	tgui::ScrollablePanel::Ptr m_matchListpanel;
+	tgui::ScrollablePanel::Ptr m_matchListCreate;
 	tgui::ListBox::Ptr listTeam1;
 	tgui::ListBox::Ptr listTeam2;
+	tgui::ListBox::Ptr listMatchCreate;
 	tgui::Button::Ptr createMatch;
+	tgui::EditBox::Ptr matchName;
 	//static void scrollPanel(tgui::Panel::Ptr panel, int value);
 	//static int previousScrollbarValue;
 
