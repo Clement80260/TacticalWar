935abaeed520022250a2ca7108047a818bda341e
Pilexlol <55394996+Pilexlol@users.noreply.github.com>
2020-12-11 15:07:26 +0100

    Add files via upload

--- AnimationsAndSoundLib/CharacterDieAction.h
+++ AnimationsAndSoundLib/CharacterDieAction.h
@@ -10,6 +10,7 @@ private:
 	bool moveAnimationFinished;
 	IScreenActionCallback * screen;
 	bool firstUpdate;
+	bool hasResetAttackAnimation;
 public:
 	CharacterDieAction(IScreenActionCallback * screen, int persoId)
 	{
@@ -17,14 +18,28 @@ public:
 		this->screen = screen;
 		moveAnimationFinished = false;
 		firstUpdate = true;
+		hasResetAttackAnimation = false;
 	}
 
 	virtual void update(float deltatime)
 	{
+		elapseTime(deltatime);
 		if (firstUpdate)
 		{
-			screen->applyCharacterDie(persoId);
+			tw::Player * srcPlayer = screen->getCharacter(persoId);
+			srcPlayer->getCharacter()->startDieAction(1);
 			firstUpdate = false;
+		}
+
+		if (getEllapsedTime() > 1000 && !hasResetAttackAnimation)
+		{
+			tw::Player * srcPlayer = screen->getCharacter(persoId);
+			srcPlayer->getCharacter()->resetAnimation();
+			hasResetAttackAnimation = true;
+
+			screen->applyCharacterDie(persoId);
+			notifyAnimationFinished(0);
+			delete this;
 		}
 	}
 
