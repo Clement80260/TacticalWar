cef407821c74f2794204a93c594eda287edd813d
florian60430 <florian.wantelez@hotmail.com>
2020-12-14 13:26:56 +0100

    IHM Selection de classe affichage nameClase, atk, pm

--- TacticalWar/LoginScreen.cpp
+++ TacticalWar/LoginScreen.cpp
@@ -199,7 +199,7 @@ void LoginScreen::onMessageReceived(std::string msg)
 		ScreenManager::getInstance()->setCurrentScreen(new BattleScreen(gui, environmentId));
 		delete this;
 	}
-	else if (sentence.substring(0, 2) == "HW")
+	else if (sentence.substring(0, 2) == "HC")
 	{
 		readyForConnect = false;
 		gui->removeAllWidgets();
